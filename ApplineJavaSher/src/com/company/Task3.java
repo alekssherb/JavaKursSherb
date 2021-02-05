d6b36301948b26474a63f9da7a8c388b778f0291
exy0n <existtolive@mail.ru>
2021-02-05 13:01:15 +0300

    Commit 3 (Задача 3 массивы )

---
+++ ApplineJavaSher/src/com/company/Task3.java
@@ -0,0 +1,12 @@
+package com.company;
+public class Task3 {
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 54, 19, 46};
+        int a = nums[0];
+        nums[0]=nums[nums.length-1];
+        nums[nums.length-1]= a;
+        int sred = nums.length/2;
+        System.out.println(nums[0] + nums[sred]);
+    }
+}
+
