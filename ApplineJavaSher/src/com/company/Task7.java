61224d463d3fafa34501f6785f66c6ac223cff31
exy0n <existtolive@mail.ru>
2021-02-13 13:15:16 +0300

    Commit 11 поправил задачу 7 на типы данных double

--- ApplineJavaSher/src/com/company/Task7.java
+++ ApplineJavaSher/src/com/company/Task7.java
@@ -2,20 +2,22 @@ package com.company;
 import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 20;
-        int z = 30;
+        double x = 10;
+        double y = 20;
+        double z = 30;
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите размер создаваемого массива");
         int size = sc.nextInt();
-        int[] array = new int[size];
-        System.out.println("Введите элементы (целые числа) массива через enter: ");
+        double[] array = new double[size];
+        System.out.println("Введите числовые элементы массива через enter: ");
         for (int i = 0; i < array.length; i++) {
-            array[i] = sc.nextInt();
+            array[i] = sc.nextDouble();
         }
-            for (int elem : array) {
+            for (double elem : array) {
                 if (elem == x || elem == y || elem == z) {
                     System.out.println(elem + " Данное значение имеется в константах");
+                } else {
+                    System.out.println(elem);
                 }
             }
         }
