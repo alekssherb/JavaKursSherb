55fbd63d41786a7cbeac2d6a9487b52355cbb6fb
exy0n <existtolive@mail.ru>
2021-02-15 12:19:03 +0300

    Commit 13 поправил 7 задачу на стринг

--- ApplineJavaSher/src/com/company/Task7.java
+++ ApplineJavaSher/src/com/company/Task7.java
@@ -2,23 +2,23 @@ package com.company;
 import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
-        double x = 10;
-        double y = 20;
-        double z = 30;
+        String x = "10";
+        String y = "20";
+        String z = "30";
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите размер создаваемого массива");
         int size = sc.nextInt();
-        double[] array = new double[size];
-        System.out.println("Введите числовые элементы массива через enter: ");
+        String[] array = new String[size];
+        System.out.println("Введите элементы массива через enter: ");
         for (int i = 0; i < array.length; i++) {
-            array[i] = sc.nextDouble();
+            array[i] = sc.nextLine();
         }
-            for (double elem : array) {
-                if (elem == x || elem == y || elem == z) {
-                    System.out.println(elem + " Данное значение имеется в константах");
-                } else {
-                    System.out.println(elem);
-                }
+        for (String elem : array) {
+            if (elem.equals(x) || elem.equals(y) || elem.equals(z)) {
+                System.out.println(elem + " Данное значение имеется в константах");
+            } else {
+                System.out.println(elem);
             }
         }
     }
+}
